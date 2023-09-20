884115d9020694677f4b6708f4b9c6b97467e04c
Bianca Stork <biancastrk@gmail.com>
2023-09-20 20:27:51 -0300

    definicion de modelos

--- database/src/database/index.js
+++ database/src/database/index.js
@@ -5,7 +5,14 @@ const { MONGO_URI} = require("../config/envs")
 const conn = mongoose.createConnection(MONGO_URI);
 
 //definimos el modelo de Personajes
-const Character = conn.model("Character", require("./schemas/characterSchema"))
+const Character = conn.model("Character", require("./schemas/characterSchema"));
+const Film = conn.model("Film", require("./schemas/filmSchema"));
+const Planet=conn.model("Planet", require("./schemas/planetSchema"));
 
 
-Character.find().then(res=>console.log(res));
+
+module.exports ={
+   Character,
+   Film,   
+   Planet
+}
