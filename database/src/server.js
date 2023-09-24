93aea5f2a4d6aebb3b516e00803d85ac9b47b44f
Bianca Stork <biancastrk@gmail.com>
2023-09-24 17:08:08 -0300

    Dockerfile database

--- database/src/server.js
+++ database/src/server.js
@@ -8,4 +8,19 @@ server.use(morgan("dev"));
 
 server.use(require("./routes"));
 
+//manejo de errores
+server.use("*", (req, res)=>{
+    res.status(404).send("Not Found"); //probar usar el manejador de errores propio
+});
+//manejador de errores propio, sobreescribiendo el manejador de errores x defecto de express
+//el manejador de errores x defecto de express envia una traza con informacino "sensible" que nunca nos deberia pasar en nuestras aplicaciones---> Nunca deberiamos enviar informacion sensible al cliente 
+server.use((err, req, res, next)=>{
+    // res.send("manejador de errores!")
+    res.status(err.statusCode || 500).send({
+        error: true,
+        message: err.message,
+    });
+});
+
+
 module.exports=server;
